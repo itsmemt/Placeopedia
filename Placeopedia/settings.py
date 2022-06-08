67d4a7f8dd3714d75f480a071f733a3357a3adbf
itsmemt <itsmeanimatormt@gmail.com>
2022-06-08 13:58:38 +0530

    fc

--- Placeopedia/settings.py
+++ Placeopedia/settings.py
@@ -114,7 +114,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT=[BASE_DIR,'static']
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
