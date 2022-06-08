e2c0c6e3db3077ccd0d119d35e42ab35a600ba93
itsmemt <itsmeanimatormt@gmail.com>
2022-06-08 13:36:50 +0530

    1

--- Placeopedia/urls.py
+++ Placeopedia/urls.py
@@ -17,7 +17,10 @@ from django.urls import include, re_path
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.contrib.auth import views
-#import company
+
+#For server
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^company/',include('company.urls')),
@@ -25,6 +28,12 @@ urlpatterns = [
     re_path(r'^contact/',include('contact.urls')),
     re_path(r'^training/',include('training.urls')),
     re_path(r'^$',include('home.urls')),
+
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    
+
 ]
 
 # Admin panel redesign
@@ -33,47 +42,3 @@ admin.site.site_title="Placement Cell Portal"
 admin.site.index_title="Admin Panel"
 
 
-# # paths for training section
-# from django.urls import  re_path as path
-# from exam import views
-# from django.contrib.auth.views import LogoutView,LoginView
-# urlpatterns = [
-   
-#     path('admin/', admin.site.urls),
-#     path('student/',include('student.urls')),
-    
-
-
-#     path('',views.home_view,name=''),
-#     path('logout', LogoutView.as_view(template_name='exam/logout.html'),name='logout'),
-#     path('contactus', views.contactus_view),
-#     path('afterlogin', views.afterlogin_view,name='afterlogin'),
-
-
-
-#     path('adminclick', views.adminclick_view),
-#     path('adminlogin', LoginView.as_view(template_name='exam/adminlogin.html'),name='adminlogin'),
-#     path('admin-dashboard', views.admin_dashboard_view,name='admin-dashboard'),
-   
-
-#     path('admin-student', views.admin_student_view,name='admin-student'),
-#     path('admin-view-student', views.admin_view_student_view,name='admin-view-student'),
-#     path('admin-view-student-marks', views.admin_view_student_marks_view,name='admin-view-student-marks'),
-#     path('admin-view-marks/<int:pk>', views.admin_view_marks_view,name='admin-view-marks'),
-#     path('admin-check-marks/<int:pk>', views.admin_check_marks_view,name='admin-check-marks'),
-#     path('update-student/<int:pk>', views.update_student_view,name='update-student'),
-#     path('delete-student/<int:pk>', views.delete_student_view,name='delete-student'),
-
-#     path('admin-course', views.admin_course_view,name='admin-course'),
-#     path('admin-add-course', views.admin_add_course_view,name='admin-add-course'),
-#     path('admin-view-course', views.admin_view_course_view,name='admin-view-course'),
-#     path('delete-course/<int:pk>', views.delete_course_view,name='delete-course'),
-
-#     path('admin-question', views.admin_question_view,name='admin-question'),
-#     path('admin-add-question', views.admin_add_question_view,name='admin-add-question'),
-#     path('admin-view-question', views.admin_view_question_view,name='admin-view-question'),
-#     path('view-question/<int:pk>', views.view_question_view,name='view-question'),
-#     path('delete-question/<int:pk>', views.delete_question_view,name='delete-question'),
-
-
-# ]
